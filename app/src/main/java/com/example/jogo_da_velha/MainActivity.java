d0004b28939f142d1d66abbfab92a9253583582f
joao-vitorps <jpaimdasilva@yahoo.com.br>
2022-06-22 22:07:48 -0300

    Quarto commit: criação dos métodos para inciar as variáveis e posições, permitir o jogador jogar e mostrar o que foi jogado

--- app/src/main/java/com/example/jogo_da_velha/MainActivity.java
+++ app/src/main/java/com/example/jogo_da_velha/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.jogo_da_velha;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    //variáveis bonitinhas
     private Button[] arrayBotoes = new Button[10];
+    private String vez = "X";
+    private int jogadas = 0;
+    private String[] posicoes = new String[10]; //posição do tabuleiro do jogo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +26,42 @@ public class MainActivity extends AppCompatActivity {
         arrayBotoes [7] = findViewById((R.id.botao7));
         arrayBotoes [8] = findViewById((R.id.botao8));
         arrayBotoes [9] = findViewById((R.id.botao9));
+        ativarBotoes();
     }
+    //método para ativar os botões - onClickButtons
+    private void ativarBotoes(){
+        for (int i = 1; i<10;i++){
+            int finalI = i;
+            arrayBotoes[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    jogada(finalI);
+                }
+            });
+            posicoes[i]=""; // ele inicializa os botoes como vazio para depois trocar
+        }
+    }
+
+    private void jogada(int x){
+        if(posicoes[x] == ""){
+            posicoes[x] = vez;
+            jogadas++;
+            if (vez == "X"){
+                vez = "O";
+            }else{
+                vez = "X";
+            }
+        }
+        exibirJogadas();
+
+    }
+
+    //método para atualizar o jogo
+    private void exibirJogadas(){
+        for (int x = 1; x<10;x++){
+            arrayBotoes[x].setText(posicoes[x]);
+        }
+    }
+
+
 }

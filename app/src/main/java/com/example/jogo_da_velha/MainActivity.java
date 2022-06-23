98c6e7e4a9d35a4dc0061f91a2055605b6feaef9
Lucas Susluaga <lucassusluaga@gmail.com>
2022-06-22 22:52:12 -0300

    Quinto commit - Inclusão de dois novos métodos verificadores, um deles verifica as combinações e outro determina quem é o ganhador dentro das combinações. Inclusão também de comentários relevantes sobre o projeto em sí

--- app/src/main/java/com/example/jogo_da_velha/MainActivity.java
+++ app/src/main/java/com/example/jogo_da_velha/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.jogo_da_velha;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,9 +12,9 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     //variáveis bonitinhas
     private Button[] arrayBotoes = new Button[10];
-    private String vez = "X";
+    private String opcao = "X"; //será criado um alerta para definir qual é a vez que o usuário escolher
     private int jogadas = 0;
-    private String[] posicoes = new String[10]; //posição do tabuleiro do jogo
+    private String[] posicoes = new String[10]; //posições do tabuleiro no jogo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,19 +44,19 @@ public class MainActivity extends AppCompatActivity {
             posicoes[i]=""; // ele inicializa os botoes como vazio para depois trocar
         }
     }
-
+    //ainda será implementado um alerta que substituirá a opção X como escolha padrão aqui ness método
     private void jogada(int x){
         if(posicoes[x] == ""){
-            posicoes[x] = vez;
+            posicoes[x] = opcao;
             jogadas++;
-            if (vez == "X"){
-                vez = "O";
+            if (opcao == "X"){
+                opcao = "O";
             }else{
-                vez = "X";
+                opcao = "X";
             }
         }
         exibirJogadas();
-
+        verifica();
     }
 
     //método para atualizar o jogo
@@ -63,5 +66,96 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //método para exibir o alerta do resultado do jogo
+    private void verificaGanhador(String ganhador){
+        //Inicia com a criação de um alerta que mostra quem ganhou ...
+        AlertDialog.Builder builder = new
+                AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Resultado");
+        if (ganhador.equals("")){
+            builder.setMessage("Empate");
+        }else{
+            if (ganhador.equals("X")){
+                builder.setMessage("\"X\" é o vencedor");
+            }else{
+                builder.setMessage("\"O\" é o vencedor");
+            }
+        }
+        /*...após mostra uma opção para começar um novo jogo que reseta as informações
+        das posições e jogadas */
+        builder.setPositiveButton("Novo jogo", new
+                DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        jogadas = 0;
+                        for (int x = 1;x<10;x++){
+                            posicoes[x]="";
+                        }
+                        exibirJogadas();
+                        opcao = "X";
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    /*método que verifica as posições das jogadas e utiliza o método verificaGanhador
+     para exibir quem ganhou
+     */
+    private void verifica(){
+        //ganhador 1ª linha
+        if(posicoes[1].equals(posicoes[2]) &&
+                posicoes[1].equals(posicoes[3]) && posicoes[1].toString() != ""){
+            verificaGanhador(posicoes[1]);
+            return;
+        }
+        //ganhador 2ª linha
+        if(posicoes[4].equals(posicoes[5]) &&
+                posicoes[4].equals(posicoes[6]) && posicoes[4].toString() != ""){
+            verificaGanhador(posicoes[4]);
+            return;
+        }
+        //ganhador 3ª linha
+        if(posicoes[7].equals(posicoes[8]) &&
+                posicoes[7].equals(posicoes[9]) && posicoes[7].toString() != ""){
+            verificaGanhador(posicoes[7]);
+            return;
+        }
+        //ganhador 1ª coluna
+        if(posicoes[1].equals(posicoes[4]) &&
+                posicoes[1].equals(posicoes[7]) && posicoes[1].toString() != ""){
+            verificaGanhador(posicoes[1]);
+            return;
+        }
+        //ganhador 2ª coluna
+        if(posicoes[2].equals(posicoes[5]) &&
+                posicoes[2].equals(posicoes[8]) && posicoes[2].toString() != ""){
+            verificaGanhador(posicoes[2]);
+            return;
+        }
+        //ganhador 3ª coluna
+        if(posicoes[3].equals(posicoes[6]) &&
+                posicoes[3].equals(posicoes[9]) && posicoes[3].toString() != ""){
+            verificaGanhador(posicoes[3]);
+            return;
+        }
+        //ganhador diagonal principal
+        if(posicoes[1].equals(posicoes[5]) &&
+                posicoes[1].equals(posicoes[9]) && posicoes[1].toString() != ""){
+            verificaGanhador(posicoes[1]);
+            return;
+        }
+        //ganhador diagonal secundária
+        if(posicoes[3].equals(posicoes[5]) &&
+                posicoes[3].equals(posicoes[7]) && posicoes[3].toString() != ""){
+            verificaGanhador(posicoes[3]);
+            return;
+        }
+        //Em caso de empate - todos campos preenchidos
+        if (jogadas == 9){
+            verificaGanhador("");
+            return;
+        }
+    }
 
 }
